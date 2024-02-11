d789c3cc018633decc2419bebf28279316b96b00
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-02-11 14:10:15 +0000

    Fixed other errors. Still need to fix the render error along with the creation of the HTML file.

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
+const team = [];
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 // This is the prmopt user function that which is calling inquirer.prompt. Below it is various questions that will be in the file.
-const promptUser = () =>
+const promptUser = () => {
   inquirer
     .prompt([
       {
@@ -48,97 +48,102 @@ const promptUser = () =>
         ],
       },
     ])
-    // Here I am pushing the manager's data that the user has entered to the HTML file.
     .then((htmlData) => {
-      const newManager = new Manager(
-        htmlData.name,
-        htmlData.id,
-        htmlData.email,
-        htmlData.officeNumber
-      );
-      this.manager.push(newManager);
+      if (team.length === 0) {
+        const newManager = new Manager(
+          htmlData.name,
+          htmlData.id,
+          htmlData.email,
+          htmlData.officeNumber
+        );
+        team.push(newManager);
+      }
+      createTeam(htmlData.employeeOptions);
+      // render(team);
     });
+};
+function createTeam(employeeOptions) {
+  if (employeeOptions === "Add an Engineer") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please enter the engineer's name",
+        },
+        {
+          type: "number",
+          name: "id",
+          message: "Please enter the engineer's employee id",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Please enter the engineer's email",
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "Please enter the engineer's github username",
+        },
+      ])
+      // Here I am pushing the Engineer's data that the user has entered to the HTML file.
+      .then((htmlData) => {
+        const newEngineer = new Engineer(
+          htmlData.name,
+          htmlData.id,
+          htmlData.email,
+          htmlData.github
+        );
+        team.push(newEngineer);
+      });
+  } else if (employeeOptions === "Add an Intern") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please enter the intern's name",
+        },
+        {
+          type: "number",
+          name: "id",
+          message: "Please enter the intern's employee id",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Please enter the intern's email",
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "Please enter the intern's school name",
+        },
+      ])
+      // Here I am pushing the Intern's data that the user has entered to the HTML file.
+      .then((htmlData) => {
+        const newIntern = new Intern(
+          htmlData.name,
+          htmlData.id,
+          htmlData.email,
+          htmlData.school
+        );
+        team.push(newIntern);
+      });
+    // If the user selects that they have finished building their team after the manager has been created. This code will trigger and will write and generate the HTML file and will also catch any errors in the process.
+  } else if (employeeOptions === "I have finished building my team.") {
+    const employeeHTML = this.generateteam(team);
+    fs.writeFile(outputPath),
+      employeeHTML,
+      (err) => {
+        if (err) throw new Error(err);
 
-if (employeeOptions === "Add an Engineer") {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Please enter the engineer's name",
-      },
-      {
-        type: "number",
-        name: "id",
-        message: "Please enter the engineer's employee id",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Please enter the engineer's email",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Please enter the engineer's github username",
-      },
-    ])
-    // Here I am pushing the Engineer's data that the user has entered to the HTML file.
-    .then((htmlData) => {
-      const newEngineer = new Engineer(
-        htmlData.name,
-        htmlData.id,
-        htmlData.email,
-        htmlData.github
-      );
-      this.engineer.push(newEngineer);
-    });
-} else if (employeeOptions === "Add an Intern") {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Please enter the intern's name",
-      },
-      {
-        type: "number",
-        name: "id",
-        message: "Please enter the intern's employee id",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Please enter the intern's email",
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "Please enter the intern's school name",
-      },
-    ])
-    // Here I am pushing the Intern's data that the user has entered to the HTML file.
-    .then((htmlData) => {
-      const newIntern = new Intern(
-        htmlData.name,
-        htmlData.id,
-        htmlData.email,
-        htmlData.school
-      );
-      this.intern.push(newIntern);
-    });
-  // If the user selects that they have finished building their team after the manager has been created. This code will trigger and will write and generate the HTML file and will also catch any errors in the process.
-} else if (employeeOptions === "I have finished building my team.") {
-  const employeeHTML = this.generateteam(team);
-  fs.writeFile(outputPath),
-    employeeHTML,
-    (err) => {
-      if (err) throw new Error(err);
-
-      console.log("Page created! Check out ./output/team.html");
-    };
+        console.log("Page created! Check out ./output/team.html");
+      };
+  }
+  render(team);
 }
 
 // Here I'm calling the promptUser method and rendering the Manager, Engineer and Intern.
 promptUser();
-render([Manager, Engineer, Intern]);
